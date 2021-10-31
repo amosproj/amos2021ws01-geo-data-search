bdd6edccf5a2e91d0119caab1a6a4b098b83ee68
Veronika Skorkina <skorkina@campus.tu-berlin.de>
2021-10-31 19:11:32 +0100

    set up frontend including docker support

---
+++ src/frontend/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Geo Data Search</title>
+      </Head>
+
+      <main>
+        <h1>Geo Data Search</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
