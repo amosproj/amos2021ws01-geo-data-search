f841eb82efdc37ec90bac82f0c3b5e89d535c9a1
erik.fischer <58076232+Battlemech@users.noreply.github.com>
2021-11-17 19:45:34 +0100

    formated string interpreter

--- src/nlp/src/api/string_interpreter.py
+++ src/nlp/src/api/string_interpreter.py
@@ -4,27 +4,30 @@ import spacy
 # load spacy ml nlp model
 nlp = spacy.load("de_core_news_sm")
 
+
 def process_string(string: str) -> str:
 
     # analyse preprocessed string
     return get_query(string).get_json()
 
-def preprocess_string(string : str) -> str:
+
+def preprocess_string(string: str) -> str:
     # analyse string
     tokens = nlp(string)
 
     # combine lemmata into string
     preprocessed_string = ""
-    
+
     for token in tokens:
         # separate tokens with space
-        if( preprocessed_string != ""):
+        if(preprocessed_string != ""):
             preprocessed_string += " "
         preprocessed_string += token.lemma_
 
     return preprocessed_string
 
-def get_query(string : str) -> object:
+
+def get_query(string: str) -> object:
 
     string = preprocess_string(string)
 
@@ -37,6 +40,7 @@ def get_query(string : str) -> object:
         result.min_height = "100 m"
 
     return result
+
 
 class Query:
     def __init__(self) -> None:
@@ -67,49 +71,49 @@ class Query:
         self.min_curves_right = ""
         self.max_curves_right = ""
         self.curve_count_right = ""
-        
+
         # left curves
         self.min_curves_left = ""
         self.max_curves_left = ""
         self.curve_count_left = ""
 
-    
     def get_json(self) -> str:
         dict = {
-            "location" : self.location,
-            "max distance" : self.max_distance,
-            "query object" : self.query_object,
-            "route attributes" : {
-                "height" : {
-                    "min" : self.min_height,
-                    "max" : self.max_height
+            "location": self.location,
+            "max distance": self.max_distance,
+            "query object": self.query_object,
+            "route attributes": {
+                "height": {
+                    "min": self.min_height,
+                    "max": self.max_height
                 },
-                "length" : {
-                    "min" : self.min_route_length,
-                    "max" : self.max_route_length
+                "length": {
+                    "min": self.min_route_length,
+                    "max": self.max_route_length
                 },
-                "gradiant" : {
-                    "min" : self.min_gradiant,
-                    "max" : self.max_gradiant,
-                    "length" : self.gradiant_length
+                "gradiant": {
+                    "min": self.min_gradiant,
+                    "max": self.max_gradiant,
+                    "length": self.gradiant_length
                 },
-                "curves" : {
-                    "min" : self.min_curves,
-                    "max" : self.max_curves,
-                    "count" : self.curve_count,
-                    "right" : {
-                        "min" : self.min_curves_right,
-                        "max" : self.max_curves_right,
-                        "count" : self.curve_count_right,
+                "curves": {
+                    "min": self.min_curves,
+                    "max": self.max_curves,
+                    "count": self.curve_count,
+                    "right": {
+                        "min": self.min_curves_right,
+                        "max": self.max_curves_right,
+                        "count": self.curve_count_right,
                     },
-                    "left" : {
-                        "min" : self.min_curves_left,
-                        "max" : self.max_curves_left,
-                        "count" : self.curve_count_left,
+                    "left": {
+                        "min": self.min_curves_left,
+                        "max": self.max_curves_left,
+                        "count": self.curve_count_left,
                     }
                 }
             },
         }
         return json.dumps(dict)
+
 
 print(process_string("Finde alle Berge in Berlin die höher als 100m sind"))
