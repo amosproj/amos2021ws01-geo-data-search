b7f5a006a899a03c30751db35b6ffb668fb214bc
nikita <nikita.khakham@vergleich.org>
2021-11-23 00:44:03 +0100

    updated python build container

--- src/nlp/tests/api/test_string_interpreter.py
+++ src/nlp/tests/api/test_string_interpreter.py
@@ -1,21 +1,20 @@
 import sys
-sys.path.insert(0, '..')
-from api import string_interpreter
 
+from api.string_interpreter import get_query
 
 def test_default_string_interpretation():
-    result = string_interpreter.get_query(
+    result = get_query(
         "Finde alle Berge in Berlin die höher als 100m sind"
     )
 
     assert result.location == "Berlin"
     assert result.query_object == "Mountain"
-    assert result.route.attributes.height.min == 100
+    assert result.route_attributes.height.min == 100
 
 
 def test_no_input():
-    result = string_interpreter.get_query("")
+    result = get_query("")
 
     assert result.location == ""
     assert result.query_object == ""
-    assert result.route.attributes.height.min == 0
+    assert result.route_attributes.height.min == 0
