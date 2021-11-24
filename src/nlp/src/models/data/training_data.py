d3afdf1edca6beb4fe89f6add3b866365d02dfe9
erik.fischer <58076232+Battlemech@users.noreply.github.com>
2021-11-24 23:52:32 +0100

    added comment

--- src/nlp/src/models/data/training_data.py
+++ src/nlp/src/models/data/training_data.py
@@ -7,25 +7,26 @@ from spacy_formatter import format_chatette_output
 # run Chatette
 os.system("python -m chatette chatette-test-01.chatette")
 SEP = os.path.sep
+PATH_SPACY = "output" + SEP
 
 
 def parse_data(name: str):
-    PATH_NLU = f"output{SEP}{name}{SEP}"
-    PATH_SPACY = "output" + SEP
+    path_nlu = f"output{SEP}{name}{SEP}"
 
     # ensure that output path exists
-    Path(PATH_NLU).mkdir(parents=True, exist_ok=True)
+    Path(path_nlu).mkdir(parents=True, exist_ok=True)
 
     # convert rasa output to a more handy one
-    files = os.listdir(PATH_NLU)
+    files = os.listdir(path_nlu)
     training_data = []
     for file in files:
-        sub_training_data = format_chatette_output(PATH_NLU + file)
+        sub_training_data = format_chatette_output(path_nlu + file)
         training_data = training_data + sub_training_data
         # write resulting json to file
 
     pickle.dump(training_data, open(PATH_SPACY + name + "_" + file, "wb"))
 
 
+# parse training and test data
 parse_data("train")
 parse_data("test")
