7a509bed46a3e45cde0fd2d1636a41f89dc05185
Veronika Skorkina <skorkina@campus.tu-berlin.de>
2021-11-24 19:58:51 +0100

    Add search results and dev mode

--- src/frontend/components/SearchView.tsx
+++ src/frontend/components/SearchView.tsx
@@ -2,11 +2,13 @@ import React, { FormEvent, useState } from 'react';
 import apiClient from '@lib/api-client';
 import SearchInput from './SearchInput';
 import ErrorMessage from './ErrorMessage';
-import { SearchQueryResponse, SearchResult } from '@lib/types/search';
+import { SearchQueryResponse, SearchResult, SearchError } from '@lib/types/search';
+import SearchListResult from './SearchListResult';
+import { isDevelopment, sampleSearchResults } from '@lib/config';
 
 const SearchView = () => {
   const [searchValue, setSearchValue] = useState('');
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [errorData, setErrorData] = useState<SearchError | null>(null);
   const [results, setResults] = useState<SearchResult[] | null>(null);
   const [loading, setLoading] = useState(false);
 
@@ -15,41 +17,91 @@ const SearchView = () => {
 
     try {
       setLoading(true);
+      setErrorData(null);
+      setResults(null);
+
       const response = await apiClient('/user_query', {
         body: {
           query: searchValue,
         },
       });
+
       if (response.ok) {
         const { result, error }: SearchQueryResponse = await response.json();
+        console.log('Search result', result);
 
         if (error) {
-          setErrorMessage(error.message);
+          setErrorData(error);
         } else if (result) {
           setResults(result);
+        } else {
+          setErrorData({
+            type: 'system',
+            message: 'Got empty response from the API',
+          });
         }
       } else {
-        setErrorMessage('Looks like the server is down');
+        setErrorData({
+          type: 'client',
+          message: 'Looks like the server is down',
+          trace: new Error(`${response.status} ${response.statusText}`).stack,
+        });
       }
     } catch (err) {
       console.error('Search error', err);
-      setErrorMessage('Something went wrong, see console log for more info');
+      setErrorData({
+        type: 'client',
+        message: 'Something went wrong',
+        trace: new Error().stack,
+      });
     } finally {
       setLoading(false);
     }
   };
+
+  const searchTermEmpty = searchValue.trim().length === 0;
 
   return (
     <div className="my-8">
       <form onSubmit={getSearchSuggestions}>
         <div className="flex mb-4">
           <SearchInput placeholder="Search" value={searchValue} onChange={setSearchValue} />
-          <button className="bg-blue-500 text-white rounded px-5 ml-2" type="submit">
+          <button
+            className="bg-blue-500 text-white rounded px-5 ml-2 disabled:opacity-70 disabled:cursor-default"
+            type="submit"
+            disabled={searchTermEmpty}
+          >
             Submit
           </button>
+
+          <button
+            className="bg-yellow-500 text-white rounded px-5 ml-2 flex-shrink-0 disabled:opacity-70 disabled:cursor-default"
+            type="button"
+            disabled={searchTermEmpty}
+            onClick={() => {
+              setErrorData(null);
+              setResults(sampleSearchResults);
+            }}
+          >
+            Get fake results
+          </button>
         </div>
-        {results && results.length === 0 && <ErrorMessage message={'No results found'} />}
-        {errorMessage && <ErrorMessage message={errorMessage} />}
+
+        {results &&
+          (results.length > 0 ? (
+            <ul className="mt-8">
+              {sampleSearchResults.map((result) => (
+                <SearchListResult key={result.id} result={result} />
+              ))}
+            </ul>
+          ) : (
+            <p className="mb-2">No results found</p>
+          ))}
+
+        {errorData?.message && <ErrorMessage message={`Error: ${errorData.message}`} />}
+        {isDevelopment && errorData?.trace && (
+          <ErrorMessage message={`Trace: ${errorData.trace}`} />
+        )}
       </form>
     </div>
   );
