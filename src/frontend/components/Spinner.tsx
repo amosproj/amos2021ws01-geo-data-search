af6a36c0f121542adab1caa893700de2af7807f1
Veronika Skorkina <skorkina@campus.tu-berlin.de>
2021-11-29 19:17:54 +0100

    Fix html attribute case

--- src/frontend/components/Spinner.tsx
+++ src/frontend/components/Spinner.tsx
@@ -18,7 +18,7 @@ const Spinner = ({ className }: SpinnerProps) => {
         cy="12"
         r="10"
         stroke="currentColor"
-        stroke-width="4"
+        strokeWidth="4"
       />
       <path
         className="opacity-75"
