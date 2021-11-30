aaf854853d6a9f8e03c0276b439b64953ffacc75
Veronika Skorkina <skorkina@campus.tu-berlin.de>
2021-11-30 20:07:18 +0100

    Embed OSM map

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '3xl': "0 0 10px rgba(0,0,0, 0.4)"
+      },
+    },
   },
   variants: {
     extend: {},
