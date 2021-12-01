dd77f5787ea629551a102072703b89405f263d6a
Numan <numanfidan@gmail.com>
2021-12-01 14:48:50 +0100

    NodeTag class created and ApiResult interface updated with requeired functions

--- src/backend/src/main/java/com/example/backend/data/ApiResult.java
+++ src/backend/src/main/java/com/example/backend/data/ApiResult.java
@@ -1,4 +1,14 @@
 package com.example.backend.data;
 
 public interface ApiResult {
+
+    public String getType();
+
+    public int getId();
+
+    public String getLat();
+
+    public String getLon();
+
+    public String getName();
 }
