b1e0637638ddc7d2549e35e3347f67333b1bef7b
Olivia Dargel <olivia.dargel@campus.tu-berlin.de>
2021-12-01 17:08:02 +0100

    changed to os independent paths

--- src/nlp/src/main.py
+++ src/nlp/src/main.py
@@ -1,14 +1,20 @@
+import os
 import subprocess
 
 from models.data.training_data import generate_data
 from models.train_model import train
 
+# get os specific file separator
+SEP = os.path.sep
+
+
 # generate training data with Chatette
-generate_data("./models/data")
+generate_data(f".{SEP}models{SEP}data")
 
 # train model with spaCy
-train(train_data_path="./models/data/output/train_output.json", model_path="./models/training")
+train(train_data_path=f".{SEP}models{SEP}data{SEP}output{SEP}train_output.json",
+      model_path=f".{SEP}models{SEP}training")
 
 # for host machine in order to be able to edit generated files
-subprocess.call(['chmod', '-R', '777', './models/data/output'])
-subprocess.call(['chmod', '-R', '777', './models/training'])
+subprocess.call(['chmod', '-R', '777', f'.{SEP}models{SEP}data{SEP}output'])
+subprocess.call(['chmod', '-R', '777', f'.{SEP}models{SEP}training'])
