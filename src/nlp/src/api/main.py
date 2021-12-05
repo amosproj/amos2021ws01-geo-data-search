6e70c95d928c00905f363f60fe402e53bc53fbf7
Olivia Dargel <olivia.dargel@campus.tu-berlin.de>
2021-12-05 01:17:22 +0100

    mv startup process to src/main.py

--- src/nlp/src/api/main.py
+++ src/nlp/src/api/main.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import pathlib
 
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
@@ -11,35 +10,12 @@ from .string_interpreter import process_string, Query
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
 
-CURRENT_DIR = pathlib.Path(__file__).parent.resolve()
-
 # get os specific file separator
 SEP = os.path.sep
 
 # App title and landing page
 APP_TITLE = "API for NLP Component"
 app = FastAPI(title=APP_TITLE)
-
-
-@app.on_event("startup")
-async def startup_event():
-    model_path = pathlib.Path(str(CURRENT_DIR.parent) + f"{SEP}models{SEP}training")
-    if model_path.exists():
-        logging.info(f"[NLP COMPONENT][API] Found model on path {model_path}")
-    else:
-        logging.info(f"[NLP COMPONENT][API] No model found on path {model_path}")
-
-        # check whether there are training data available
-        data_path = pathlib.Path(str(CURRENT_DIR.parent) + f"{SEP}models{SEP}data{SEP}output{SEP}train{SEP}output.json")
-        if data_path.exists():
-            # no training data generation needed
-            logging.info(f"[NLP COMPONENT][API] Found training data {data_path}")
-
-        else:
-            logging.info(f"[NLP COMPONENT][API] Could not find training data file {data_path}")
-            # TODO generate training data
-        # TODO train model
-    # TODO maybe load model
 
 
 @app.get("/", include_in_schema=False, response_class=HTMLResponse)
@@ -64,6 +40,4 @@ async def get_version():
 )
 async def request(text: str):
     logging.info(f"[NLP Component] Received Request: {text}")
-    text = "Finde alle Berge in Berlin die höher als 100m sind"  # TODO remove
-
     return process_string(text)
