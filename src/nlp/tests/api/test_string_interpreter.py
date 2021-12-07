d795204c651a2ceb0a42673bf1f5e0a3766e2cb5
Olivia Dargel <olivia.dargel@campus.tu-berlin.de>
2021-12-07 01:39:28 +0100

    added tests for query_object keywords

--- src/nlp/tests/api/test_string_interpreter.py
+++ src/nlp/tests/api/test_string_interpreter.py
@@ -12,21 +12,36 @@ def test_default_string_interpretation():
 
 
 def test_route_keyword():
-    assert "route" == get_query("").query_object
-    assert "route" == get_query("").query_object
-    assert "route" == get_query("").query_object
+    result = get_query("Zeige mir einen Weg von Essen nach Köln")
+    assert result.query_object == "route"
+
+    result = get_query("Gibt's Routen von der Arktis zur Antarktis")
+    assert result.query_object == "route"
+
+    result = get_query("Straße ab Bremershaven")
+    assert result.query_object == "route"
 
 
 def test_place_keyword():
-    assert "place" == get_query("").query_object
-    assert "place" == get_query("").query_object
-    assert "place" == get_query("").query_object
+    result = get_query("Ort in NRW")
+    assert result.query_object == "place"
+
+    result = get_query("Zeige mir Seen auf der Mecklenburger Seenplatte")
+    assert result.query_object == "place"
+
+    result = get_query("Wo liegt der bayrische Wald")
+    assert result.query_object == "place"
 
 
 def test_elevation_keyword():
-    assert "elevation" == get_query("").query_object
-    assert "elevation" == get_query("").query_object
-    assert "elevation" == get_query("").query_object
+    result = get_query("Gibt es hohe Hügel in Bayern")
+    assert result.query_object == "elevation"
+
+    result = get_query("Erhebungen in Mecklenburg-Vorpommern")
+    assert result.query_object == "elevation"
+
+    result = get_query("Zeige mir Berge in Hamburg")
+    assert result.query_object == "elevation"
 
 
 def test_default_keyword():
@@ -44,5 +59,5 @@ def test_no_input():
     result = get_query("")
 
     assert result.location == ""
-    assert result.query_object == ""
+    assert result.query_object == "route"  # route is default value
     assert result.route_attributes.height.min == 0
