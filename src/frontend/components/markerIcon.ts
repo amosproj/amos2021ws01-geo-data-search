4f24c048463b4d9b3ed8dc97cd7e464efe9e5538
Veronika Skorkina <skorkina@campus.tu-berlin.de>
2021-12-08 21:34:04 +0100

    Add map markers and recalc the map position when search results are updated

---
+++ src/frontend/components/markerIcon.ts
@@ -0,0 +1,9 @@
+import Leaflet from 'leaflet';
+
+const markerIcon = Leaflet.icon({
+  iconUrl: '/images/map-marker.svg',
+  iconSize: [28, 42],
+  iconAnchor: [14, 0],
+});
+
+export default markerIcon;
