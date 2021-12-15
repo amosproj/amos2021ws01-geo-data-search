45fcef6840709ec72622b2b970d214a8f7ab52fa
NebiMucaj <mucaj.nebi@gmail.com>
2021-12-15 18:00:03 +0100

    #112 cleaned up unit tests/added new tests

--- src/nlp/tests/api/test_string_interpreter.py
+++ src/nlp/tests/api/test_string_interpreter.py
@@ -1,4 +1,77 @@
 from api.string_interpreter import get_query
+import pytest
+
+
+def get_query_objects_test_data():
+    queries = [
+        ["Finde alle Berge in Berlin die höher als 100m sind", "elevation"],
+        ["Zeige mir einen Weg von Essen nach Köln", "route"],
+        ["Gibt's Routen von der Arktis zur Antarktis", "route"],
+        ["Ort in NRW", "place"],
+        ["Zeige mir Seen auf der Mecklenburger Seenplatte", "place"],
+        ["Wo liegt der bayrische Wald", "place"],
+        ["Finde alle Berge in Berlin die höher als 100m sind", "elevation"],
+        ["Finde alle Berge in Berlin die höher als 100m sind", "elevation"],
+        ["Finde alle Berge in Berlin die höher als 100m sind", "elevation"],
+        ["Finde alle Berge in Berlin die höher als 100m sind", "elevation"],
+    ]
+    return queries
+
+
+def get_gradient_test_data():
+    queries = [
+        ["Zeige mir einen Weg von Essen nach Köln mit einer Steigung von maximal 7", "max", 7],
+        ["Plane mir eine Route nach Paris mit einer Steigung von über 30%", "min", 30],
+        ["Plane mir eine Route nach Paris mit einem Anteil von 500 meter Steigung von maximal 9%", "max", 9,],
+        ["Plane mir eine Route nach Paris mit einer Steigung von mindestens 22%","min", 22],
+        ["Plane mir eine Route nach Paris mit einer Steigung von 8%", "min", 8],
+    ]
+    return queries
+
+
+def get_keyword_test_data():
+    queries = [
+        ["Plane mir eine Route nach Paris mit einer Steigung von maximal 7%", "route"],
+        ["Gibt's Routen von der Arktis zur Antarktis", "route"],
+        ["Straße ab Bremershaven", "route"],
+        ["Ort in NRW", "place"],
+        ["Zeige mir Seen auf der Mecklenburger Seenplatte", "place"],
+        ["Wo liegt der bayrische Wald", "place"],
+        ["Gibt es hohe Hügel in Bayern", "elevation"],
+        ["Erhebungen in Mecklenburg-Vorpommern", "elevation"],
+        ["Zeige mir Berge in Hamburg", "elevation"],
+        ["Wo sind Almen in Brandenburg", "route"],
+        ["Restaurants in Spanien", "route"],
+        ["Lage des Kieler Hafens", "route"]
+
+    ]
+    return queries
+
+
+@pytest.mark.parametrize("query", get_query_objects_test_data())
+def test_query_objects(query):
+    result = get_query(query[0])
+    assert result.query_object == query[1]
+
+@pytest.mark.parametrize("query", get_gradient_test_data())
+def test_route_gradient(query):
+    result = get_query(query[0])
+    assert getattr(result.route_attributes.gradiant, query[1]) == query[2]
+
+@pytest.mark.parametrize("query", get_keyword_test_data())
+def test_query_objects_keyword(query):
+    result = get_query(query[0])
+    assert result.query_object == query[1]
+
+def test_default_keyword():
+    result = get_query("Wo sind Almen in Brandenburg")
+    assert result.query_object == "route"
+
+    result = get_query("Restaurants in Spanien")
+    assert result.query_object == "route"
+
+    result = get_query("Lage des Kieler Hafens")
+    assert result.query_object == "route"
 
 
 def test_default_string_interpretation():
@@ -17,51 +90,6 @@ def test_long_string_interpretation():
     assert result.location == "Italien"
     assert result.query_object == "route"
     assert result.route_attributes.height.max == 10000
-
-
-def test_route_keyword():
-    result = get_query("Zeige mir einen Weg von Essen nach Köln")
-    assert result.query_object == "route"
-
-    result = get_query("Gibt's Routen von der Arktis zur Antarktis")
-    assert result.query_object == "route"
-
-    result = get_query("Straße ab Bremershaven")
-    assert result.query_object == "route"
-
-
-def test_place_keyword():
-    result = get_query("Ort in NRW")
-    assert result.query_object == "place"
-
-    result = get_query("Zeige mir Seen auf der Mecklenburger Seenplatte")
-    assert result.query_object == "place"
-
-    result = get_query("Wo liegt der bayrische Wald")
-    assert result.query_object == "place"
-
-
-def test_elevation_keyword():
-    result = get_query("Gibt es hohe Hügel in Bayern")
-    assert result.query_object == "elevation"
-
-    result = get_query("Erhebungen in Mecklenburg-Vorpommern")
-    assert result.query_object == "elevation"
-
-    result = get_query("Zeige mir Berge in Hamburg")
-    assert result.query_object == "elevation"
-
-
-def test_default_keyword():
-    result = get_query("Wo sind Almen in Brandenburg")
-    assert result.query_object == "route"
-
-    result = get_query("Restaurants in Spanien")
-    assert result.query_object == "route"
-
-    result = get_query("Lage des Kieler Hafens")
-    assert result.query_object == "route"
-
 
 def test_convert_units():
     result = get_query("Zeige mir Berge in Hamburg mit einer Höhe von 1 kilometern")
@@ -89,23 +117,3 @@ def test_route_length():
 
     assert result.route_attributes.length.min == 100
     assert result.route_attributes.length.max == 1000
-
-
-def test_route_gradient():
-    result = get_query(
-        "Plane mir eine Route nach Paris mit einer Steigung von maximal 7%"
-    )
-
-    assert result.route_attributes.gradiant.max == 7
-
-    result = get_query(
-        "Plane mir eine Route nach Paris mit einer Steigung von über 7% "
-    )
-
-    assert result.route_attributes.gradiant.min == 7
-
-    result = get_query(
-        "Plane mir eine Route nach Paris mit einem Anteil von 500 meter Steigung von maximal 7%"
-    )
-
-    assert result.route_attributes.gradiant.max == 7
