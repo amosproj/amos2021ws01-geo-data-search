46bc937dbd9073b3a6ace2b7a4612feb24809ea9
Olivia Dargel <olivia.dargel@campus.tu-berlin.de>
2022-01-12 20:10:50 +0100

    added log path

--- src/nlp/src/main.py
+++ src/nlp/src/main.py
@@ -1,4 +1,10 @@
 import logging
+logging.basicConfig(format='%(asctime)s %(levelname)s : %(name)s : %(message)s',
+                    datefmt='%d.%m.%Y %H:%M:%S',
+                    encoding='utf-8',
+                    level=logging.DEBUG)
+LOGGER = logging.getLogger("src.main")
+
 import os
 import pathlib
 import subprocess
@@ -14,18 +20,18 @@ CURRENT_DIR = pathlib.Path(__file__).parent.resolve()
 # check if model already exists
 model_path = pathlib.Path(str(CURRENT_DIR) + f"{SEP}models{SEP}training")
 if model_path.exists():
-    logging.info(f"[NLP COMPONENT] Found model on path {model_path}")
+    LOGGER.info("Found model on path %s", model_path)
 else:
-    logging.info(f"[NLP COMPONENT] No model found on path {model_path}")
+    LOGGER.info("No model found on path %s", model_path)
 
     # check whether there are training data available
     data_path = pathlib.Path(str(CURRENT_DIR) + f"{SEP}models{SEP}data{SEP}output{SEP}train_output.json")
     if data_path.exists():
         # no training data generation needed
-        logging.info(f"[NLP COMPONENT] Found training data {data_path}")
+        LOGGER.info("Found training data %s", data_path)
 
     else:
-        logging.info(f"[NLP COMPONENT] Could not find training data file {data_path}")
+        LOGGER.info("Could not find training data file %s", data_path)
 
         # generate training data with Chatette
         generate_data(str(CURRENT_DIR) + f"{SEP}models{SEP}data")
@@ -48,3 +54,15 @@ else:
         subprocess.call(['chmod', '-R', '777',  path])
     else:
         os.chmod(path, 0o777)
+
+# prepare API log path
+log_path = pathlib.Path(f'{CURRENT_DIR}{SEP}api{SEP}logs{SEP}')
+if not log_path.exists():
+    LOGGER.info("No log path found on path %s", log_path)
+    pathlib.Path(log_path).mkdir(parents=True, exist_ok=True)
+    if SEP == "/":
+        subprocess.call(['chmod', '-R', '777',  log_path])
+    else:
+        os.chmod(log_path, 0o777)
+
+    LOGGER.info("Created log path %s", log_path)
