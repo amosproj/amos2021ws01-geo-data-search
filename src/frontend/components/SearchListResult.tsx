1588ac768f46dc8f94cc107e4154b34675b293e3
Veronika Skorkina <skorkina@campus.tu-berlin.de>
2022-01-22 22:10:48 +0100

    Show routes on map

--- src/frontend/components/SearchListResult.tsx
+++ src/frontend/components/SearchListResult.tsx
@@ -1,28 +1,40 @@
 import { SearchResult } from '@lib/types/search';
 import MarkerIcon from '@assets/svg/marker.svg';
+import MarkerRouteIcon from '@assets/svg/marker-route.svg';
 import { getSearchResultDescription } from '@lib/search';
+import { cc } from '@lib/common';
 
 type Props = {
   result: SearchResult;
   onClick?: (result: SearchResult) => void;
+  isRoutePart?: boolean;
 };
 
-const SearchListResult = ({ result, onClick }: Props) => {
+const SearchListResult = ({ result, onClick, isRoutePart }: Props) => {
   const handleResultClick = () => {
     onClick?.(result);
   };
 
   return (
     <li
-      aria-label='Select search result'
-      className="p-2 flex items-center hover:bg-[#E6F2F8] rounded cursor-pointer"
+      aria-label="Select search result"
+      className={cc([
+        'p-2 flex items-center hover:bg-[#E6F2F8] cursor-pointer',
+        isRoutePart
+          ? 'first:rounded-b-none last:rounded-t-none first:rounded-t last:rounded-b'
+          : 'rounded',
+      ])}
       onClick={handleResultClick}
       role="button"
     >
-      <MarkerIcon className="h-5 mr-3 flex-shrink-0" />
-      <div>
-        <h3 className="font-bold">{result.name}</h3>
-        <p className="text-sm">
+      {isRoutePart && result.type !== 'Finish' ? (
+        <MarkerRouteIcon className="h-3 w-3 mr-3 flex-shrink-0 z-10" />
+      ) : (
+        <MarkerIcon className="h-5 mr-3 flex-shrink-0" />
+      )}
+      <div className="overflow-hidden">
+        <h3 className="font-bold truncate">{result.name}</h3>
+        <p className="text-sm truncate">
           <span className="capitalize">{result.type}</span> ·{' '}
           <span className="text-gray-500">{getSearchResultDescription(result)}</span>
         </p>
--- src/frontend/components/SearchResults.tsx
+++ src/frontend/components/SearchResults.tsx
@@ -2,27 +2,60 @@ import { useAtom } from 'jotai';
 import { currentSearchResultAtom } from '@lib/store';
 import { SearchResult } from '@lib/types/search';
 import SearchListResult from './SearchListResult';
+import { useMemo } from 'react';
 
 const SearchResults = ({ results }: { results: SearchResult[] | null }) => {
   const [, setCurrentSearchResult] = useAtom(currentSearchResultAtom);
 
+  const isRoute = useMemo(() => {
+    return results ? results.length >= 2 && results.some((i) => !!i.polyline) : false;
+  }, [results]);
+
+  const renderSearchResults = () => {
+    return (
+      results &&
+      results.map((result, index) => (
+        <SearchListResult
+          key={`${result.id}${index}`}
+          result={result}
+          onClick={setCurrentSearchResult}
+          isRoutePart={isRoute}
+        />
+      ))
+    );
+  };
+
+  if (!results) {
+    return null;
+  }
+
+  if (results.length === 0) {
+    return <p className="mb-2">No results found</p>;
+  }
+
+  if (isRoute) {
+    const start = results.find((i) => i.type === 'Start');
+    const finish = results.find((i) => i.type === 'Finish');
+
+    return (
+      <div className="">
+        <h2 className="text-lg mt-2">
+          Route from <span className="font-bold">{start?.name}</span> to{' '}
+          <span className="font-bold">{finish?.name}</span>
+        </h2>
+
+        <ul aria-label="Search results" className="sm:mt-2 bg-white rounded results--route">
+          {renderSearchResults()}
+        </ul>
+      </div>
+    );
+  }
+
+  // Regular search results
   return (
-    <>
-      {results &&
-        (results.length > 0 ? (
-          <ul aria-label='Search results' className="sm:mt-4">
-            {results.map((result, index) => (
-              <SearchListResult
-                key={`${result.id}${index}`}
-                result={result}
-                onClick={setCurrentSearchResult}
-              />
-            ))}
-          </ul>
-        ) : (
-          <p className="mb-2">No results found</p>
-        ))}
-    </>
+    <ul aria-label="Search results" className="sm:mt-4">
+      {renderSearchResults()}
+    </ul>
   );
 };
 
