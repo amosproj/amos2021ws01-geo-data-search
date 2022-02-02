81527cdf24964a3ca842a04b24168d1f70f8faa8
Numan <numanfidan@gmail.com>
2022-02-02 21:36:41 +0100

    KMLPlaceMark and KMLRoute classes implemented

--- src/backend/src/main/java/com/example/backend/data/kml/KMLPlaceMark.java
+++ src/backend/src/main/java/com/example/backend/data/kml/KMLPlaceMark.java
@@ -1,112 +1,58 @@
 package com.example.backend.data.kml;
 
 import com.example.backend.data.ApiResult;
-import org.jdom2.Document;
 import org.jdom2.Element;
-import org.jdom2.Namespace;
-import org.jdom2.output.Format;
-import org.jdom2.output.XMLOutputter;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
-public class KMLPlaceMark implements KML {
-    private Document doc;
-    private List<PlaceMark> placeMarks;
+public class KMLPlaceMark extends KML {
 
     private KMLPlaceMark(Builder builder) {
-        setDoc(builder.getDoc());
+        super(builder);
         setPlaceMarks(builder.getPlaceMarks());
     }
 
-    public Document getDoc() {
-        return doc;
-    }
-
-    public void setDoc(Document doc) {
-        this.doc = doc;
-    }
-
-    public List<PlaceMark> getPlaceMarks() {
-        return placeMarks;
-    }
-
-    public void setPlaceMarks(List<PlaceMark> placeMarks) {
-        this.placeMarks = placeMarks;
-    }
-
-    public static class Builder {
-        private final Namespace namespace = Namespace.getNamespace("http://www.opengis.net/kml/2.2");
-        private Element rootDoc;
-        private Document doc;
-        private List<PlaceMark> placeMarks;
-
+    public static class Builder extends KML.Builder {
         public Builder() {
-            doc = new Document();
-            Element root = new Element("kml", namespace);
-            rootDoc = new Element("Document", namespace);
-            root.addContent(rootDoc);
-            doc.setRootElement(root);
-            placeMarks = new ArrayList<PlaceMark>();
-        }
-
-        public Document getDoc() {
-            return doc;
-        }
-
-        public List<PlaceMark> getPlaceMarks() {
-            return placeMarks;
+            super();
         }
 
         public Builder addPlaceMark(PlaceMark placeMarkObj) {
-            placeMarks.add(placeMarkObj);
-            Element placeMarkElement = new Element("Placemark", namespace);
-            Element nameElement = new Element("name", namespace).setText(placeMarkObj.getName());
+            getPlaceMarks().add(placeMarkObj);
+            Element placeMarkElement = new Element("Placemark", getNamespace());
+            Element nameElement = new Element("name", getNamespace()).setText(placeMarkObj.getName());
             placeMarkElement.addContent(nameElement);
 
-            Element descriptionElement = new Element("description", namespace).setText(placeMarkObj.getDescription());
+            Element descriptionElement = new Element("description", getNamespace()).setText(placeMarkObj.getDescription());
             placeMarkElement.addContent(descriptionElement);
 
-            Element point = new Element("Point", namespace);
-            Element coordinates = new Element("coordinates", namespace);
+            Element point = new Element("Point", getNamespace());
+            Element coordinates = new Element("coordinates", getNamespace());
             coordinates.setText(placeMarkObj.getCoordinates());
             point.addContent(coordinates);
 
             placeMarkElement.addContent(point);
-            rootDoc.addContent(placeMarkElement);
+            getRootDoc().addContent(placeMarkElement);
             return this;
         }
 
-        public KMLPlaceMark build() {
+        @Override
+        public KML build() {
             return new KMLPlaceMark(this);
         }
 
-        public KMLPlaceMark from(ApiResult apiResult) {
-            PlaceMark placeMark = new PlaceMark(apiResult.getName(), apiResult.getType(), apiResult.getLat(), apiResult.getLon());
-            return addPlaceMark(placeMark).build();
+        @Override
+        protected KML forRoute(List<ApiResult> apiResultList) {
+            return null;
         }
 
-        public KMLPlaceMark from(List<ApiResult> apiResultList) {
-            if (apiResultList == null || apiResultList.isEmpty()) return null;
+        @Override
+        protected KML forElevation(List<ApiResult> apiResultList) {
             for (ApiResult apiResult : apiResultList) {
                 PlaceMark placeMark = new PlaceMark(apiResult.getName(), apiResult.getType(), apiResult.getLat(), apiResult.getLon());
                 addPlaceMark(placeMark);
             }
             return this.build();
         }
-    }
-
-    @Override
-    public String toString() {
-        XMLOutputter xmlOutputter = new XMLOutputter();
-        // pretty print
-        xmlOutputter.setFormat(Format.getPrettyFormat());
-        try {
-            xmlOutputter.output(getDoc(), System.out);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return xmlOutputter.outputString(getDoc());
     }
 }
