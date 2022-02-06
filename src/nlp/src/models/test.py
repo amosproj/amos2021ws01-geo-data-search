781ccc7e237d46a6b1def02ceef9be79982010af
Olivia Dargel <olivia.dargel@campus.tu-berlin.de>
2022-02-06 20:30:34 +0100

    deleted file for local tests

--- src/nlp/src/models/test.py
+++
@@ -1,9 +0,0 @@
-import spacy
-
-
-nlp = spacy.load("./training")
-
-doc = nlp("zeige mir 2000 seen in Berlin")
-print(nlp.pipe_names)
-for ent in doc.ents:
-    print(ent.text, ent.start_char, ent.end_char, ent.label_)
